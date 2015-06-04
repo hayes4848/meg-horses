9fb18e556f549f298d3cd8c90d61b1949157ad7a
Andy Hayes <andy.hayes@SAX-LT-AHAYES.local>
2015-06-04 16:35:56 -0600

    adds s3 bucket stuff

--- client/templates/singleHorse/singleHorse.js
+++ client/templates/singleHorse/singleHorse.js
@@ -1,19 +1,15 @@
-Template.singleHorse.helpers({
-	images: function () {
-		return Images.find();
-	}
-})
-
-
 Template.singleHorse.events({
-	'change .myFileInput': function(event, template) {
-    var files = event.target.files;
-    for (var i = 0, ln = files.length; i < ln; i++) {
-      Images.insert(files[i], function (err, fileObj) {
+  'change .myFileInput': function(event, template) {
+    FS.Utility.eachFile(event, function(file) {
+      Images.insert(file, function (err, fileObj) {
         // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
-        
-        console.log(Session.get('horse'));
       });
-    }
+    });
   }
+});
+
+Template.singleHorse.helpers({
+    images: function () {
+        return Images.find();
+    }
 })
