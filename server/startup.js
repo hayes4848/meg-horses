e1690a838e4bb7996ccf4f946ff37fb3110ea02e
Andy Hayes <andy.hayes@AHAYES.local>
2015-12-13 23:44:23 -0700

    trying this

--- server/startup.js
+++ server/startup.js
@@ -3,5 +3,6 @@ if (Meteor.isServer) {
 		// code to run on server at startup
 		process.env.MOBILE_DDP_URL = 'http://megan_horse.meteor.com';
 		process.env.MOBILE_ROOT_URL = 'http://megan_horse.meteor.com';
+		DDP_DEFAULT_CONNECTION_URL= 'http://megan_horse.meteor.com';
 	});
 }
