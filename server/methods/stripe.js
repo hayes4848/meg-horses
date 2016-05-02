6a5a8db4b112ee9b5fb5ae1fc2b321a006412abf
Andy Hayes <andythemanhayes@hotmail.com>
2016-05-01 20:51:38 -0600

    upgrades

--- server/methods/stripe.js
+++ server/methods/stripe.js
@@ -1,44 +1,44 @@
-var secret = Meteor.settings.private.stripe.testSecretKey;
-var Stripe = StripeAPI(secret);
+// var secret = Meteor.settings.private.stripe.testSecretKey;
+// var Stripe = StripeAPI(secret);
 	
 
-Meteor.methods({
+// Meteor.methods({
 
 
-	stripeCreateCustomer: function (token, email) {
-	  // Note: we'd check() both of our arguments here, but I've stripped this out for the sake of brevity.
+// 	stripeCreateCustomer: function (token, email) {
+// 	  // Note: we'd check() both of our arguments here, but I've stripped this out for the sake of brevity.
 
-	  var stripeCustomer = new Future();
+// 	  var stripeCustomer = new Future();
 
-	  Stripe.customers.create({
-	    source: token,
-	    email: email
-	  }, function(error, customer){
-	    if (error){
-	      stripeCustomer.return(error);
-	    } else {
-	      stripeCustomer.return(customer);
-	    }
-	  });
+// 	  Stripe.customers.create({
+// 	    source: token,
+// 	    email: email
+// 	  }, function(error, customer){
+// 	    if (error){
+// 	      stripeCustomer.return(error);
+// 	    } else {
+// 	      stripeCustomer.return(customer);
+// 	    }
+// 	  });
 
-	  return stripeCustomer.wait();
-	}, 
+// 	  return stripeCustomer.wait();
+// 	}, 
 
-	stripeCreateSubscription: function(customer, plan){
-	  // Again, we'd do a check() here. Don't skip it!
+// 	stripeCreateSubscription: function(customer, plan){
+// 	  // Again, we'd do a check() here. Don't skip it!
 
-	  var stripeSubscription = new Future();
+// 	  var stripeSubscription = new Future();
 
-	  Stripe.customers.createSubscription(customer, {
-	    plan: plan
-	  }, function(error, subscription){
-	    if (error) {
-	      stripeSubscription.return(error);
-	    } else {
-	      stripeSubscription.return(subscription);
-	    }
-	  });
+// 	  Stripe.customers.createSubscription(customer, {
+// 	    plan: plan
+// 	  }, function(error, subscription){
+// 	    if (error) {
+// 	      stripeSubscription.return(error);
+// 	    } else {
+// 	      stripeSubscription.return(subscription);
+// 	    }
+// 	  });
 
-	  return stripeSubscription.wait();
-	}
-});
+// 	  return stripeSubscription.wait();
+// 	}
+// });
